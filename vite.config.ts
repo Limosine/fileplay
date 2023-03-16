34c0d9e98ed28937f3febc0c6e0324e8dd986ecb
Leonhard Masche <leonhardmasche@gmail.com>
2023-03-16 23:38:37 +0100

    wip workbox pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,41 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { SvelteKitPWA } from "@vite-pwa/sveltekit";
+import type { ConfigEnv, UserConfig } from "vite";
+import { generateImages } from "pwa-asset-generator";
+import type { ManifestOptions } from "vite-plugin-pwa";
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+export default async function (config: ConfigEnv): Promise<UserConfig> {
+  const { savedImages, htmlMeta, manifestJsonContent } = await generateImages(
+    "src/logo.png",
+    "static/pwa-assets/",
+    {
+      darkMode: true,
+      favicon: true,
+      path: "/pwa-assets/",
+      scrape: false,
+    }
+  );
+
+  return {
+    plugins: [
+      sveltekit(),
+      SvelteKitPWA({
+        srcDir: "src",
+        filename: "sw.ts",
+        registerType: "autoUpdate",
+        strategies: "injectManifest",
+        useCredentials: true,
+        devOptions: {
+          enabled: true,
+          type: "module",
+          navigateFallback: "/",
+        },
+        manifest: Object.assign(
+          {},
+          await import("./src/manifest.json"),
+          manifestJsonContent
+        ) as unknown as ManifestOptions,
+      }),
+    ],
+  };
+}
