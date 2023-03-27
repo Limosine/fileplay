5f472315878b75193803f7986c55b4090c1e1ef2
leonhma <leonhardmasche@gmail.com>
2023-03-27 20:52:29 +0200

    Add neode schema for neo4j

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,13 +1,13 @@
 import Neode from "neode";
-import { NEO4J_URL, NEO4J_USER, NEO4J_PASSWORD } from "$env/static/private";
+import { NEO4J_URL, NEO4J_USERNAME, NEO4J_PASSWORD } from "$env/static/private";
 import User from '$lib/server/models/User'
-import Contacts from '$lib/server/models/Contacts'
+import Device from '$lib/server/models/Device'
 
 export function createNeode(): Neode {
-  const neode = new Neode(NEO4J_URL, NEO4J_USER, NEO4J_PASSWORD);
+  const neode = new Neode(NEO4J_URL, NEO4J_USERNAME, NEO4J_PASSWORD);
   neode.with({
     User,
-    Contacts
+    Device
   })
   return neode;
 }
