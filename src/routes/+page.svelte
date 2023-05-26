e04bad0e3d05094023e76dd0ed2e918f57e28676
Quentin Frey <51170829+Limosine@users.noreply.github.com>
2023-05-27 00:27:16 +0200

    Improve PeerJS module + Add PeerJS functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,11 @@
   import { Icon } from "@smui/common";
   import Card, { PrimaryAction } from "@smui/card";
   import Input, { input, files } from "../lib/components/Input.svelte";
-  import { goto } from "$app/navigation";
   import SetupDialog from '$lib/dialogs/SetupDialog.svelte';
+  import SendDialog from "$lib/dialogs/SendDialog.svelte";
   import { onMount } from "svelte";
   import { browser } from "$app/environment";
+  import { writable } from "svelte/store";
 
   const handleDrop = (e: DragEvent) => {
     e.preventDefault();
@@ -15,11 +16,18 @@
     $files = e.dataTransfer.files;
   };
   let setup_open = false;
+  let send_open = false;
 
-  onMount(() => {
+  let sender_uuid = writable<string>();
+  let recieved_files = writable<{ url: string, name: string }[]>([]);
+
+  onMount(async () => {
     if (browser && !localStorage.getItem("setupDone")) {
       setup_open = true;
     }
+
+    sender_uuid = (await import('$lib/peerjs')).sender_uuid;
+    recieved_files = (await import('$lib/peerjs')).recieved_files;
   });
 </script>
 
@@ -27,8 +35,14 @@
 
 <Input />
 <SetupDialog open={setup_open} />
+<SendDialog open={send_open} />
 
 <div class="center">
+  {#if $sender_uuid}
+    <Card padded>
+      <h3>My uuid: {$sender_uuid}</h3>
+    </Card>
+  {/if}
   <div class="beside">
     <Card>
       <PrimaryAction on:click={() => $input.click()} style="padding: 64px">
@@ -40,11 +54,11 @@
     {#if $files}
       <Card>
         <PrimaryAction
-          on:click={() => goto("/contacts/select")}
+          on:click={() => send_open = true}
           style="padding: 64px"
         >
-          <Icon class="material-icons" style="font-size: 30px">contacts</Icon>
-          Select contact(s)
+          <Icon class="material-icons" style="font-size: 30px">send</Icon>
+          Send file(s)
         </PrimaryAction>
       </Card>
     {/if}
@@ -57,6 +71,17 @@
 
       {#each Array.from($files) as file}
         <p>{file.name}</p>
+      {/each}
+    </Card>
+  {/if}
+
+  {#if $recieved_files.length != 0}
+    <Card padded>
+      <h4>Recieved file(s):</h4>
+      <p class="small"><br /></p>
+
+      {#each $recieved_files as recieved_file}
+        <a href={recieved_file.url} download={recieved_file.name}>{recieved_file.name}</a><br/>
       {/each}
     </Card>
   {/if}
