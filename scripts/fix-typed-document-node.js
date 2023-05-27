43159db06f6af2e06557384bc51fcf08dcfb96f0
Quentin Frey <51170829+Limosine@users.noreply.github.com>
2023-05-27 12:04:12 +0200

    Remove GraphQL packages, files and scripts

--- scripts/fix-typed-document-node.js
+++
@@ -1,5 +0,0 @@
-import { readFileSync, writeFileSync } from 'fs';
-
-const file = readFileSync('./src/lib/gql.ts', 'utf8')
-const newFile = file.replace(/import { TypedDocumentNode/, 'import type { TypedDocumentNode')
-writeFileSync('./src/lib/gql.ts', newFile, 'utf8')
