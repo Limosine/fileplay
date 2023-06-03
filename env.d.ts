59323084c786dc057b2045c79c4dbfa3f3ff3b85
leonhma <leonhardmasche@gmail.com>
2023-06-03 23:07:10 +0200

    fix dotenv?

--- env.d.ts
+++ env.d.ts
@@ -1,4 +1,3 @@
-declare module "dotenv-vault-core"
 declare module "virtual:pwa-assets"
 declare module "virtual:pwa-register"
 declare module "virtual:pwa-info"
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { SvelteKitPWA } from "@vite-pwa/sveltekit";
 import type { ConfigEnv, UserConfig } from "vite";
-import { config } from "dotenv-vault-core";
+import { config } from "dotenv";
 import PWAAssets, { getManifestIcons } from "./scripts";
 
 config();
