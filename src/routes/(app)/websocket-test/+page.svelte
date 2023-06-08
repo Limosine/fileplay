bbf8e5999b47096778f83b7bc84c3bce81de7ea2
Quentin Frey <51170829+Limosine@users.noreply.github.com>
2023-06-08 13:39:56 +0200

    Websocket Test (3)

---
+++ src/routes/(app)/websocket-test/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import Button from "@smui/button";
+  import { onMount } from "svelte";
+
+
+  let websocket: WebSocket;
+  onMount(() => {
+    websocket = new WebSocket('wss://dev.fileplay.pages.dev/websocket-t2');
+    websocket.addEventListener('message', event => {
+    console.log('Message received from server');
+    console.log(event.data);
+    });
+  });
+
+  const message = () => {
+    websocket.send("isOnline");
+  }
+
+</script>
+<Button on:click={message}>
+  SendMessage
+</Button>
