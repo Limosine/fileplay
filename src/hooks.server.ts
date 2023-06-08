03319bd1919609427edc00513e9b15b2d0e1b102
Quentin Frey <51170829+Limosine@users.noreply.github.com>
2023-06-08 09:47:43 +0200

    Test WebSockets

---
+++ src/hooks.server.ts
@@ -0,0 +1,26 @@
+/** @type {import('@sveltejs/kit').Handle} */
+export async function handle({ event, resolve }) {
+  if (event.url.pathname.startsWith('/websocket')) {
+
+    const upgradeHeader = event.request.headers.get('Upgrade');
+    if (!upgradeHeader || upgradeHeader !== 'websocket') {
+      return new Response('Expected Upgrade: websocket', { status: 426 });
+    }
+
+    const webSocketPair = new WebSocketPair();
+    const [client, server] = Object.values(webSocketPair);
+  
+    server.accept();
+    server.addEventListener('message', event => {
+      console.log(event.data);
+    });
+
+    return new Response(null, {
+      status: 101,
+      webSocket: client,
+    });
+  }
+
+  const response = await resolve(event);
+  return response;
+}
