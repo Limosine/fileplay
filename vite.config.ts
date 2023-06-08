207002b818421d0b81138b533ad3909d79f210c7
Leonhard Masche <leonhardmasche@gmail.com>
2023-06-08 18:09:48 +0200

    fix?

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,18 @@ import { SvelteKitPWA } from "@vite-pwa/sveltekit";
 import type { ConfigEnv, UserConfig } from "vite";
 import { config } from "dotenv";
 import { ONLINE_STATUS_REFRESH_TIME } from "./src/lib/common";
+import EnvironmentPlugin from "vite-plugin-environment";
 
 config();
 
 export default async function (config: ConfigEnv): Promise<UserConfig> {
   return {
     define: {
-      '>PUBLIC_VAPID_KEY<': process.env.PUBLIC_VAPID_KEY,
-      '>ONLINE_STATUS_REFRESH_TIME<': ONLINE_STATUS_REFRESH_TIME
+      ">PUBLIC_VAPID_KEY<": process.env.PUBLIC_VAPID_KEY,
+      ">ONLINE_STATUS_REFRESH_TIME<": ONLINE_STATUS_REFRESH_TIME,
     },
     plugins: [
+      EnvironmentPlugin(["NODE_ENV"]),
       sveltekit(),
       SvelteKitPWA({
         srcDir: "src",
