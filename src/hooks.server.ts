9a79bb92b5ed5ff5e29cb3b2687032d1cd50620f
Quentin Frey <51170829+Limosine@users.noreply.github.com>
2023-06-08 12:35:55 +0200

    Reverse last commits

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,3 @@
-import { COOKIE_SIGNING_SECRET } from '$env/static/private';
-import { loadKey, loadSignedDeviceID } from '$lib/server/crypto';
-import { createKysely } from '$lib/server/db';
-
-
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
   if (event.url.pathname.startsWith('/websocket')) {
@@ -12,49 +7,13 @@ export async function handle({ event, resolve }) {
       return new Response('Expected Upgrade: websocket', { status: 426 });
     }
 
-    const db = createKysely(event.platform);
-    const key = await loadKey(COOKIE_SIGNING_SECRET);
-    const { did } = await loadSignedDeviceID(event.cookies, key, db);
-
     const webSocketPair = new WebSocketPair();
     const [client, server] = Object.values(webSocketPair);
   
     server.accept();
-    server.addEventListener('message', async (event) => {
+    server.addEventListener('message', event => {
       server.send("Message recieved.")
-      if (event.data == "isOnline") {
-        server.send("Got request.");
-
-        const update = {isOnline: 1};
-
-        const res = await db
-          .updateTable("devices")
-          .set(update)
-          .where(({ cmpr }) =>
-            cmpr("did", "=", did)
-          )
-          .returning("did")
-          .executeTakeFirst();
-
-        if (!res) server.send("ERROR: 'Failed to update device info'");
-      }
     });
-
-    // server.addEventListener('close', () => {
-    //   onlineStatus(0, db, server, did);
-    //   const update = {isOnline: 0};
-
-    //   const res = await db
-    //     .updateTable("devices")
-    //     .set(update)
-    //     .where(({ cmpr }) =>
-    //       cmpr("did", "=", did)
-    //     )
-    //     .returning("did")
-    //     .executeTakeFirst();
-
-    //   if (!res) server.send("ERROR: 'Failed to update device info'");
-    // });
 
     return new Response(null, {
       status: 101,
