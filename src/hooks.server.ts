1d05fa619b8497d6cbb994e3e7e7db4b52f2d913
Quentin Frey <51170829+Limosine@users.noreply.github.com>
2023-06-08 12:08:16 +0200

    Add WebSockets

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,3 +1,24 @@
+import { COOKIE_SIGNING_SECRET } from '$env/static/private';
+import { loadKey, loadSignedDeviceID } from '$lib/server/crypto';
+import { createKysely } from '$lib/server/db';
+import type { Database } from '$lib/db';
+
+async function onlineStatus(status: number, db: Database, server: any, did: number)  {
+  const update = {isOnline: status};
+
+  const res = await db
+    .updateTable("devices")
+    .set(update)
+    .where(({ cmpr }) =>
+      cmpr("did", "=", did)
+    )
+    .returning("did")
+    .executeTakeFirst();
+
+  if (!res) server.send("ERROR: 'Failed to update device info'");
+}
+
+
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
   if (event.url.pathname.startsWith('/websocket')) {
@@ -7,13 +28,23 @@ export async function handle({ event, resolve }) {
       return new Response('Expected Upgrade: websocket', { status: 426 });
     }
 
+    const db = createKysely(event.platform);
+    const key = await loadKey(COOKIE_SIGNING_SECRET);
+    const { did } = await loadSignedDeviceID(event.cookies, key, db);
+
     const webSocketPair = new WebSocketPair();
     const [client, server] = Object.values(webSocketPair);
   
     server.accept();
     server.addEventListener('message', event => {
-      console.log(event.data);
       server.send("Message recieved.")
+      if (event.data == "isOnline") {
+        server.send("Got request.");
+        onlineStatus(1, db, server, did);
+      }
+    });
+    server.addEventListener('close', () => {
+      onlineStatus(0, db, server, did);
     });
 
     return new Response(null, {
