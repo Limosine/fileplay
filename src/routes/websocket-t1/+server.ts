4d23b0ba84771d4d84ac1c99fe791ac77c1f8384
Quentin Frey <51170829+Limosine@users.noreply.github.com>
2023-06-08 13:10:31 +0200

    Websocket Test

---
+++ src/routes/websocket-t1/+server.ts
@@ -0,0 +1,42 @@
+import { COOKIE_SIGNING_SECRET } from "$env/static/private";
+import { loadKey, loadSignedDeviceID } from "$lib/server/crypto";
+import { createKysely } from "$lib/server/db";
+import type { RequestHandler } from "./$types";
+
+export const GET: RequestHandler = async ({ request, cookies, platform }) => {
+  // get all devices linked to this account (requires cookie auth)
+  const db = createKysely(platform);
+  const key = await loadKey(COOKIE_SIGNING_SECRET);
+  const { did, uid } = await loadSignedDeviceID(cookies, key, db);
+
+  const upgradeHeader = request.headers.get('Upgrade');
+  if (!upgradeHeader || upgradeHeader !== 'websocket') {
+    return new Response('Expected Upgrade: websocket', { status: 426 });
+  }
+
+  const webSocketPair = new WebSocketPair();
+  const [client, server] = Object.values(webSocketPair);
+
+  server.accept();
+  server.addEventListener('message', async (event) => {
+    server.send("Message recieved.")
+
+    const update = {isOnline: 1};
+
+    const res = await db
+      .updateTable("devices")
+      .set(update)
+      .where(({ cmpr }) =>
+        cmpr("did", "=", did)
+      )
+      .returning("did")
+      .executeTakeFirst();
+
+    if (!res) server.send("ERROR: 'Failed to update device info'");
+  });
+
+  return new Response(null, {
+    status: 101,
+    webSocket: client,
+  });
+};
---
+++ src/routes/websocket/+server.ts
@@ -0,0 +1,29 @@
+import { COOKIE_SIGNING_SECRET } from "$env/static/private";
+import { loadKey, loadSignedDeviceID } from "$lib/server/crypto";
+import { createKysely } from "$lib/server/db";
+import type { RequestHandler } from "./$types";
+
+export const GET: RequestHandler = async ({ request, cookies, platform }) => {
+  // get all devices linked to this account (requires cookie auth)
+  const db = createKysely(platform);
+  const key = await loadKey(COOKIE_SIGNING_SECRET);
+  const { did, uid } = await loadSignedDeviceID(cookies, key, db);
+
+  const upgradeHeader = request.headers.get('Upgrade');
+  if (!upgradeHeader || upgradeHeader !== 'websocket') {
+    return new Response('Expected Upgrade: websocket', { status: 426 });
+  }
+
+  const webSocketPair = new WebSocketPair();
+  const [client, server] = Object.values(webSocketPair);
+
+  server.accept();
+  server.addEventListener('message', event => {
+    server.send("Message recieved.")
+  });
+
+  return new Response(null, {
+    status: 101,
+    webSocket: client,
+  });
+};
