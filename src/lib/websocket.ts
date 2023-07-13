4e0fb801abafe29fe6a6f4a1af7ed7bb3ecd1f25
Limosine <51170829+Limosine@users.noreply.github.com>
2023-07-13 12:39:23 +0200

    Create websocket.ts

---
+++ src/lib/websocket.ts
@@ -0,0 +1,34 @@
+import { readable, writable } from "svelte/store";
+
+function createWebSocket() {
+  let websocket = new WebSocket("wss://app.fileplay.me/websocket");
+  websocket.onopen = (event) => {
+    websocket.send("isOnline");
+  };
+  websocket.onclose = (event) => {
+    status.set("0");
+    console.log("WebSocket connection closed, retrying in 5 seconds.");
+    setTimeout(() => {websocket = createWebSocket()}, 5000);
+  };
+  return websocket;
+};
+
+export const socketStore = readable<"0" | "1" | "2">("0", set => {
+  const store = createWebSocket();
+  store.onmessage = (event) => {
+    if (event.data == "1" || event.data == "2") {
+      status.set(event.data);
+      set(event.data);
+    } else {
+      console.log(event.data);
+    }
+  };
+
+  setInterval(() => {
+    store.send("ping");
+  }, 25000)
+
+  return () => store.close();
+});
+
+export const status = writable<"0" | "1" | "2">("0");
