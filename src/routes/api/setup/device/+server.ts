7edfecfd3a33241cb124859584514be450f5549b
Quentin Frey <51170829+Limosine@users.noreply.github.com>
2023-07-14 10:20:45 +0200

    Fix setup

--- src/routes/api/setup/device/+server.ts
+++ src/routes/api/setup/device/+server.ts
@@ -1,40 +1,33 @@
 import { COOKIE_SIGNING_SECRET } from "$env/static/private";
 import { loadKey, saveSignedDeviceID } from "$lib/server/crypto";
 import { createKysely } from "$lib/server/db";
-import { error, json } from "@sveltejs/kit";
+import { error } from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
 import type { DeviceType } from "$lib/common";
-import { nanoid } from "nanoid";
 
 export const POST: RequestHandler = async ({ platform, request, cookies }) => {
   // post device info here, create a new device (sets cookie auth)
   const db = createKysely(platform);
 
-  const { displayName, type } = (await request.json()) as {
+   const { displayName, type, encryptionPublicKey } = (await request.json()) as {
     displayName: string;
     type: DeviceType;
+    encryptionPublicKey: string;
   };
 
   // insert new device into db
   const res = await db
     .insertInto("devices")
-    .values({ displayName, type })
+    .values({ displayName, type, encryptionPublicKey })
     .returning("did")
     .executeTakeFirst();
   if (!res) throw error(500, "Failed to create new device");
-
-  // create keepalive code
-  const { code } = await db
-    .insertInto("keepAliveCodes")
-    .values({ code: nanoid(), did: res.did })
-    .returning("code")
-    .executeTakeFirstOrThrow();
 
   // save device id in hmac signed cookie
   const key = await loadKey(COOKIE_SIGNING_SECRET);
   await saveSignedDeviceID(res.did, cookies, key);
 
-  return json({ keepAliveCode: code }, { status: 201 });
+  return new Response(null, { status: 201 });
 };
 
 // next step in onboarding: either redeem an advertisement code or create a new user using POST /api/setup/user
