8bf17d9d71ba35fd08f20dd27868660e45224134
Quentin Frey <51170829+Limosine@users.noreply.github.com>
2023-07-16 11:43:07 +0200

    Fix D1 Error (3)

--- src/routes/api/contacts/devices/+server.ts
+++ src/routes/api/contacts/devices/+server.ts
@@ -16,7 +16,7 @@ export const GET: RequestHandler = async ({ cookies, platform }) => {
       .selectFrom("contacts")
       .innerJoin("users", "contacts.a", "users.uid")
       .select([
-        "users.uid",
+        "contacts.cid"
       ])
       .where("contacts.b", "=", uid)
       .union(
@@ -24,23 +24,23 @@ export const GET: RequestHandler = async ({ cookies, platform }) => {
           .selectFrom("contacts")
           .innerJoin("users", "contacts.b", "users.uid")
           .select([
-            "users.uid",
+            "contacts.cid"
           ])
           .where("contacts.a", "=", uid)
       )
       .innerJoin("devices", "users.uid", "devices.uid")
-      .select(["contacts.cid", "devices.did", "devices.type", "devices.displayName", "devices.peerJsId", "devices.encryptionPublicKey"])
+      .select(["devices.did", "devices.type", "devices.displayName", "devices.peerJsId", "devices.encryptionPublicKey"])
       .where(({ and, cmpr }) => and([cmpr("devices.isOnline", "=", 1), cmpr("devices.lastSeenAt", ">", (dayjs().unix() - 30))]))
       .orderBy("displayName")
       .execute();
 
-      return json(devices, { status: 200 });
+    return json(devices, { status: 200 });
   } catch (e: any) {
     console.log({
       message: e.message,
       cause: e.cause.message,
     });
 
-    return new Response(e.cause.message, { status: 200 });
+    return new Response(e.cause.message, { status: 500 });
   }
 };
