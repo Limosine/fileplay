b6c8c41bbba01c71d17f33aad50c7f91a902f109
Quentin Frey <51170829+Limosine@users.noreply.github.com>
2023-09-30 17:38:22 +0200

    Fix WebSocket connection?

--- src/lib/lib/websocket.ts
+++ src/lib/lib/websocket.ts
@@ -2,38 +2,37 @@ import { get, writable } from "svelte/store";
 import { PUBLIC_FILEPLAY_DOMAIN } from "$env/static/public";
 import { ONLINE_STATUS_REFRESH_TIME } from "./common";
 
-const createWebSocketListeners = () => {
-  socketStore.update((websocket) => {
-    websocket.onopen = () => {
-      websocket.send("isOnline");
-    };
-    websocket.onclose = () => {
-      status.set("0");
-      websocketCounter.update((counter) => (counter = counter + 1));
-      if (get(websocketCounter) < 3) {
-        console.log("WebSocket connection closed, retrying in 5 seconds.");
-        setTimeout(() => {
-          socketStore.set(createWebSocket());
-        }, 5000);
-      } else {
-        clearInterval(get(interval));
-        console.log("WebSocket connection closed.");
-      }
-    };
-    websocket.onmessage = (event) => {
-      if (event.data == "1" || event.data == "2") {
-        status.set(event.data);
-      } else {
-        console.log(event.data);
-      }
-    };
-    return websocket;
-  });
+const createWebSocketListeners = (websocket: WebSocket) => {
+  websocket.onopen = () => {
+    initialization_completed.set(true);
+    websocket.send("isOnline");
+  };
+  websocket.onclose = () => {
+    status.set("0");
+    websocketCounter.update((counter) => (counter = counter + 1));
+    if (get(websocketCounter) < 3) {
+      console.log("WebSocket connection closed, retrying in 5 seconds.");
+      setTimeout(() => {
+        socketStore.set(createWebSocket());
+      }, 5000);
+    } else {
+      clearInterval(get(interval));
+      console.log("WebSocket connection closed.");
+    }
+  }; 
+  websocket.onmessage = (event) => {
+    if (event.data == "1" || event.data == "2") {
+      status.set(event.data);
+    } else {
+      console.log(event.data);
+    }
+  };
+  return websocket;
 };
 
 const createWebSocket = () => {
-  const websocket = new WebSocket(`wss://${PUBLIC_FILEPLAY_DOMAIN}/websocket`);
-  createWebSocketListeners();
+  let websocket = new WebSocket(`wss://${PUBLIC_FILEPLAY_DOMAIN}/websocket`);
+  websocket = createWebSocketListeners(websocket);
 
   return websocket;
 };
@@ -41,6 +40,8 @@ const createWebSocket = () => {
 const createInterval = () => {
   interval.set(
     setInterval(() => {
+      if (!get(initialization_completed)) return;
+
       if (
         get(socketStore).readyState == 3 ||
         get(socketStore).readyState == 2
@@ -61,6 +62,7 @@ const createSocketStore = () => {
   return createWebSocket();
 };
 
+const initialization_completed = writable(false);
 const websocketCounter = writable(0);
 const interval = writable<NodeJS.Timeout>();
 
