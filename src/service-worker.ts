24a43369afea699b440f7acff962f4ba18758155
Quentin Frey <51170829+Limosine@users.noreply.github.com>
2023-12-09 15:25:31 +0100

    Test file-handler

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,6 +1,9 @@
+/// <reference types="@sveltejs/kit" />
 /// <reference no-default-lib="true"/>
 /// <reference lib="esnext" />
 /// <reference lib="webworker" />
+
+const sw = /** @type {ServiceWorkerGlobalScope} */ (/** @type {unknown} */ (self));
 
 import {
   pageCache,
@@ -30,23 +33,50 @@ async function addFileToIDB(file: File) {
 }
 
 self.addEventListener("fetch", (event: any) => {
+  // Only use this event listener for POST requests sent to /share-file-handler.
+  const url = new URL(event.request.url);
   if (
-    event.request.url.endsWith("/receive-files/") &&
-    event.request.method === "POST"
+    event.request.method !== "POST" ||
+    url.pathname !== "/share-file-handler"
   ) {
-    return event.respondWith(
-      (async () => {
-        const formData = await event.request.formData();
-        const fileArray = formData.getAll("file") as Array<File>;
-
-        await Promise.all(
-          fileArray.map(async (file) => {
-            await addFileToIDB(file);
-          }),
-        );
-
-        return Response.redirect("./?share-target", 303);
-      })(),
-    );
+    return;
   }
+  
+  event.respondWith(
+    (async () => {
+	  // Get the data from the submitted form.
+	  const formData = await event.request.formData();
+  
+	  // Get the submitted files.
+	  const files = formData.getAll("files");
+  
+	  // Send the files to the frontend app.
+	  // sendFilesToFrontend(files);
+  
+	  // Redirect the user to a URL that shows the imported files.
+	  return Response.redirect("/display-new-files", 303);
+	})(),
+  );
 });
+
+// self.addEventListener("fetch", (event: any) => {
+//   if (
+//     event.request.url.endsWith("/receive-files/") &&
+//     event.request.method === "POST"
+//   ) {
+//     return event.respondWith(
+//       (async () => {
+//         const formData = await event.request.formData();
+//         const fileArray = formData.getAll("file") as Array<File>;
+
+//         await Promise.all(
+//           fileArray.map(async (file) => {
+//             await addFileToIDB(file);
+//           }),
+//         );
+
+//         return Response.redirect("./?share-target", 303);
+//       })(),
+//     );
+//   }
+// });
