f2508a12a16f8613acf31557eb3ad6a26bea3a29
Quentin Frey <51170829+Limosine@users.noreply.github.com>
2025-02-04 18:25:58 +0100

    infra: add dkim to octodns config

--- octodns.nix
+++ octodns.nix
@@ -48,6 +48,10 @@ in {
       (txt_record "v=DMARC1\\;  p=none\\; rua=mailto:63ec8eaa9db1457d8ddc29922bfbe366@dmarc-reports.cloudflare.net")
     ];
 
+    "cf2024-1._domainkey" = [
+      (txt_record "v=DKIM1\\; h=sha256\\; k=rsa\\; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAiweykoi+o48IOGuP7GR3X0MOExCUDY/BCRHoWBnh3rChl7WhdyCxW3jgq1daEjPPqoi7sJvdg5hEQVsgVRQP4DcnQDVjGMbASQtrY4WmB1VebF+RPJB2ECPsEDTpeiI5ZyUAwJaVX7r6bznU67g7LvFq35yIo4sdlmtZGV+i0H4cpYH9+3JJ78km4KXwaf9xUJCWF6nxeD+qG6Fyruw1Qlbds2r85U9dkNDVAS3gioCvELryh1TxKGiVTkg4wqHTyHfWsp7KD3WQHYJn0RyfJJu6YEmL77zonn7p2SRMvTMP3ZEXibnC9gz3nnhR6wcYL8Q7zXypKTMD58bTixDSJwIDAQAB")
+    ];
+
     "api" = [ a_record ];
     "api-dev" = [ a_record ];
 
